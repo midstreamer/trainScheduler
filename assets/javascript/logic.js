d116e4ee00c097391b275e18fc1c6e0166da9e0f
midstreamer <chadalessi@yahoo.com>
2018-03-16 10:29:34 -0700

    added comments to the logic.js file

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -13,7 +13,7 @@ $(function () {
 
     var trainData = firebase.database(); //assigns the value of the firebase.database function to the trainData variable
 
-    $("#newTrain").click(function () {
+    $("#newTrain").click(function () { //when the submit btton is clicked it assigns the value of the data input by the user in the form to the new variables below.
         var trainName = $("#trainName-input").val().trim();
         var destination = $("#destination-input").val().trim();
         var startTime = moment($("#startTime-input").val().trim(), "HH:mm").subtract(10, "years").format("X");
@@ -21,14 +21,14 @@ $(function () {
 
 
 
-        var newTrain = {
+        var newTrain = { //this takes the individual variables from above and assigns them to the single newTrain variable so we can push this data into the new train data.
             name: trainName,
             destination: destination,
             startTime: startTime,
             frequency: frequency
         }
 
-        trainData.ref().push(newTrain); //this pushes the 
+        trainData.ref().push(newTrain); //this pushes the new newTrain data that was added by the user in the form into trainData.ref 
 
         $("#trainName-input").val("");
         $("#destination-input").val("");
@@ -36,15 +36,15 @@ $(function () {
         $("#frequency-input").val("");
     })
 
-    trainData.ref().on("child_added", function (snapshot) {
+    trainData.ref().on("child_added", function (snapshot) { //when a new child is added this will take a snapshot of the data 
         var trainName = snapshot.val().name;
         var destination = snapshot.val().destination;
         var frequency = snapshot.val().frequency;
         var startTime = snapshot.val().startTime;
 
-        var remainder = moment().diff(moment.unix(startTime), "minutes") % frequency;
-        var minutes = frequency - remainder;
-        var arrival = moment().add(minutes, "m").format("hh:mm A");
+        var remainder = moment().diff(moment.unix(startTime), "minutes") % frequency; //this takes the current time and converts it unix then return the remainder of it and the frequency.
+        var minutes = frequency - remainder; //returns the difference between the frequency and the remainder 
+        var arrival = moment().add(minutes, "m").format("hh:mm A"); //this adds the formating for the arrival variable 
 
         console.log("trainName", trainName);
         console.log("Start Time: ", startTime);
@@ -54,7 +54,7 @@ $(function () {
 
         //append the train data to the html
 
-        $("#schedule").append(
+        $("#schedule").append( //this appends the data to the html table for display 
             "<tr><td>"+trainName+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td>")
     })
    
